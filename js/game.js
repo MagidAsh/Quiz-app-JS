b497c00de23e69e603605171a662d99401a13fba
Majid Ash <Magid.Ash.Si@gmail.com>
2024-06-11 08:17:42 -0700

    Getting data from API

--- js/game.js
+++ js/game.js
@@ -1,1 +1,20 @@
-console.log("object");
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  console.log(json);
+
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
