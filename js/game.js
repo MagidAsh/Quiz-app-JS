bb8b0ce9d61ee45098c9cc7038bf2e34bcea5093
Majid Ash <Magid.Ash.Si@gmail.com>
2024-06-11 10:30:20 -0700

    Show questions and answers to the user

--- js/game.js
+++ js/game.js
@@ -2,11 +2,15 @@ import formatData from "./helper.js";
 
 const loader = document.getElementById("loader");
 const container = document.getElementById("container");
+const questionText = document.getElementById("question-text");
+const answerList = document.querySelectorAll(".answer-text");
 
 const URL =
   "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
 
 let formattedData = null;
+let questionIndex = 0;
+let correctAnswer = null;
 
 const fetchData = async () => {
   const response = await fetch(URL);
@@ -20,6 +24,24 @@ const fetchData = async () => {
 const start = () => {
   loader.style.display = "none";
   container.style.display = "block";
+  showQuestion();
 };
 
+const showQuestion = () => {
+  const { question, answers, correctAnswerIndex } =
+    formattedData[questionIndex];
+
+  correctAnswer = correctAnswerIndex;
+  questionText.innerText = question;
+  answerList.forEach((button, index) => {
+    button.innerText = answers[index];
+  });
+};
+
+const checkAnswer = () => {};
+
 window.addEventListener("load", fetchData);
+
+answerList.forEach((button, index) => {
+  button.addEventListener("click", checkAnswer);
+});
